6b13200e68f3b78a9b1c2901ff9e4c2ddb4c9df3
Li <li@gmail.com>
2022-11-02 22:30:05 +0300

    feat: finish first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path')
+
+const filepath = path.join(__dirname, '', 'text.txt');
+
+fs.readFile(filepath, 'utf-8', (err, content) => {
+    if (err) {
+        throw err
+    }
+    console.log(content);
+})

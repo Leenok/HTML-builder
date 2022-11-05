a936774fe9601e6d0de52b0b1819f49309258adf
Li <li@gmail.com>
2022-11-05 12:49:44 +0300

    feat: finish task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,17 @@
-import { hi } from './say.js';
-/*
-function hi() {
-    console.log("Hi");
-}
-*/
-hi();
+const { stdin, stdout } = process;
+const fs = require('fs')
+const path = require('path')
+
+fs.open(path.join(__dirname, 'text.txt'), 'w', (err) => {
+    if (err) {
+        throw err;
+    }
+})
+
+stdout.write("Write something...\n");
+stdin.on('data', data => {
+    const dataString = data.toString();
+    fs.appendFile(path.join(__dirname, 'text.txt'), dataString, (err) => {
+        if (err) throw err;
+    })
+});

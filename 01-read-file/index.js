736443c4ac288c8a273a341de8c4e46d2cb7fd51
Li <li@gmail.com>
2022-11-09 06:18:47 +0300

    fix: write stram

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
-const path = require('path')
+const path = require('path');
 
 const filepath = path.join(__dirname, '', 'text.txt');
+const stream = new fs.ReadStream(filepath, { encoding: 'utf-8' });
 
-fs.readFile(filepath, 'utf-8', (err, content) => {
-    if (err) {
-        throw err
-    }
-    console.log(content);
+stream.on('readable', function () {
+    let data = stream.read();
+    console.log(data);
+})
+stream.on('end', function () {
+    console.log('');
 })
